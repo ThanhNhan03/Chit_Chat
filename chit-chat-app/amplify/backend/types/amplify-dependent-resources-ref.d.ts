245958c807a90de569d6823c354fbb1b11eb3a6b
JOSEE <nhan321hd7e@gmail.com>
2024-11-12 10:35:58 +0700

    Update amplify config

--- chit-chat-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ chit-chat-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "chitchat": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
   "api": {
     "chitchat": {
       "GraphQLAPIEndpointOutput": "string",
