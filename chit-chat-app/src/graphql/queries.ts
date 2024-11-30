92b7579c53e950d9c04a5826e2238cea0577b567
Gia Hoang <giahoang29102002@gmail.com>
2024-11-30 15:21:10 +0700

    Fix UI for FriendRequest button and border

--- chit-chat-app/src/graphql/queries.ts
+++ chit-chat-app/src/graphql/queries.ts
@@ -8,7 +8,6 @@ type GeneratedQuery<InputType, OutputType> = string & {
   __generatedQueryOutput: OutputType;
 };
 
-
 export const getGroupChat = /* GraphQL */ `query GetGroupChat($id: ID!) {
   getGroupChat(id: $id) {
     id
